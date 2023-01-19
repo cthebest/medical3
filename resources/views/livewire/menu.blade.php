2176919feb0d1819c10169d8c43d35ce59588484
cthebest <119713971+cthebest@users.noreply.github.com>
2023-01-19 18:19:35 -0500

    Update menu.blade.php

--- resources/views/livewire/menu.blade.php
+++ resources/views/livewire/menu.blade.php
@@ -1,4 +1,4 @@
-<div class="h-4/5">
+<div class="h-3/4">
     <header class="bg-header-clinic bg-no-repeat bg-cover h-full w-full">
         <nav class="flex items-center justify-between bg-[#006699] text-white px-2 py-4 fixed w-full shadow">
             <div class="">
@@ -18,5 +18,13 @@
 
             </div>
         </nav>
+
+        <div class="w-48 pt-36 md:pt-36 lg:pt-52 sm:w-1/3 lg:w-1/3 px-6">
+            <h1 class="text-2xl sm:text-3xl md:text-4xl font-semibold">¿Necesitas ayuda con un integrante de la familia?</h1>
+            <span class="block">No dudes en contactarnos</span>
+            <div class="mt-2">
+                <a class="bg-[#79b320] p-2 rounded-full" href="#">Reservar cita</a>
+            </div>
+        </div>
     </header>
 </div>
