b8dad1099de6f2284293092a3c6e6de8c20b0928
cthebest <119713971+cthebest@users.noreply.github.com>
2023-01-20 17:47:00 -0500

    Update index-service.blade.php

--- Modules/Service/Resources/views/livewire/index-service.blade.php
+++ Modules/Service/Resources/views/livewire/index-service.blade.php
@@ -2,7 +2,8 @@
     @if ($services->count() > 0)
         <section class=" bg-orange-100">
             <div class="container mx-auto space-y-4 py-10">
-                <h1 class="text-[#006699] text-4xl">Nuestros servicios</h1>
+                <h1 class="text-[#006699] text-4xl">¡Acércate a nosotros para informarte, descubrir un posible
+                    trastorno o problema neurólogico, es importante realizar un estudio a fondo!</h1>
 
                 <div class="grid md:grid-cols-2 lg:grid-cols-3 sm:grid-cols-1 py-10 gap-2">
                     @foreach ($services as $service)
